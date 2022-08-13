8b00c6cf8d5dca855d748c032e90c5d8bfea351b
didental <pr0besmile@gmail.com>
2022-08-13 18:16:10 +0900

    - 정렬 순서 확인

--- test/pageobjects/search-result.page.ts
+++ test/pageobjects/search-result.page.ts
@@ -3,6 +3,14 @@ import { ChainablePromiseElement } from 'webdriverio';
 import Page from './page';
 
 
+export interface Store {
+  name: string;
+  star: number;
+  reviewNum: number;
+  minPrice: number;
+  minDeiliveryTime: number;
+}
+
 class SearchResultPage extends Page {
   /**
    * define selectors using getter methods
@@ -10,22 +18,71 @@ class SearchResultPage extends Page {
   public get showCategoryBtn() {
     return $('#category-menu');
   }
+
   public get categories() {
     return $$(`ul > li > span.category-name.ng-binding`);
   }
 
-  public async isCategoryHidden() {
-    return await (await this.showCategoryBtn).isClickable()
+  public get sortSelectBox() {
+    return $('.list-option > div > select');
+  }
+
+  public async toggleCategory() {
+    if (await (await this.showCategoryBtn).isClickable()) {
+      await (await this.showCategoryBtn).click();
+    }
   }
 
   public async getCategoryNames() {
-    if (this.isCategoryHidden()) {
-      await (await this.showCategoryBtn).click();
-    }
+    await this.toggleCategory();
 
     let categories = await this.categories.map(el => el.getText());
     // console.log(categories);
+
+    await this.toggleCategory();
     return categories;
+  }
+
+  /**
+   * 
+   * @param by 기본 정렬순 / 별점순 / 리뷰 많은순 / 최소 주문 금액순 / 거리순 / 배달 시간순
+   */
+  public async sortBy(by: string) {
+    const availableBy = ['기본 정렬순', '별점순', '리뷰 많은순', '최소 주문 금액순', '거리순', '배달 시간순'];
+    if (availableBy.indexOf(by) < 0) {
+      console.log(`정렬 방법 오류: ${by}`);
+      console.log(`가능한 정렬 방법: ${availableBy}`);
+      return;
+    }
+
+    await (await this.sortSelectBox).selectByVisibleText(by);
+  }
+
+  public async getStoresInfo() {
+    const storeBoxSelector = '.item.clearfix';
+    const storeNameSelector = `div[ng-bind='restaurant.name']`;
+    const storeStarSelector = `.ico-star1.ng-binding`;
+    const storeReviewNumSelector = `.review_num.ng-binding`;
+    const storeMinPriceSelector = `.min-price.ng-binding`;
+    const storeDeiliveryTimeSelector = `.delivery-time.ng-binding`;
+
+    await (await $(storeBoxSelector)).waitForExist();
+    let storeBoxes = await $$(storeBoxSelector);
+    let storeInfos: Store[] = []
+    for await (let storeBox of storeBoxes) {
+      let name = await (await storeBox.$(storeNameSelector)).getText();
+      let starText = await (await storeBox.$(storeStarSelector)).getText();
+      let star = Number.parseFloat(starText.replace(/[^\d\.]*/g, ''));
+      let reviewText = await (await storeBox.$(storeReviewNumSelector)).getText();
+      let reviewNum = Number.parseInt(reviewText.replace(/[^\d\.]*/g, ''));
+      let minPriceText = await (await storeBox.$(storeMinPriceSelector)).getText();
+      let minPrice = Number.parseInt(minPriceText.replace(/[^\d\.]*/g, ''));
+      let deiliveryTimeText = await (await storeBox.$(storeDeiliveryTimeSelector)).getText();
+      let minDeiliveryTime = Number.parseInt(deiliveryTimeText.split('~')[0]);
+      storeInfos.push({ name, star, reviewNum, minPrice, minDeiliveryTime });
+    }
+
+    return storeInfos;
   }
 
 
--- test/specs/srp.e2e.ts
+++ test/specs/srp.e2e.ts
@@ -1,5 +1,30 @@
 import MainPage from '../pageobjects/main.page';
+import searchResultPage from '../pageobjects/search-result.page';
 import SearchResultPage from '../pageobjects/search-result.page';
+
+function isDescendingArray(array: number[]) {
+  let prevValue = array[0]
+  return array.every((value) => {
+    if (value > prevValue) {
+      return false;
+    } else {
+      prevValue = value;
+      return true;
+    }
+  });
+}
+
+function isAscendingOrder(array: number[]) {
+  let prevValue = array[0]
+  return array.every((value) => {
+    if (value < prevValue) {
+      return false;
+    } else {
+      prevValue = value;
+      return true;
+    }
+  });
+}
 
 describe('My Login application', () => {
   before(async () => {
@@ -9,7 +34,8 @@ describe('My Login application', () => {
   });
 
   it('Should display catogories', async () => {
-    const categoryShouldDisplay = await SearchResultPage.isCategoryHidden() ?
+
+    const categoryShouldDisplay = (await browser.getWindowSize()).width < 992 ?
       [
         '음식점 전체보기', '1인분 주문', '프랜차이즈', '치킨',
         '피자/양식', '중국집', '한식', '일식/돈까스',
@@ -24,6 +50,15 @@ describe('My Login application', () => {
     expect(categoryDisplayed).toStrictEqual(categoryShouldDisplay);
   });
 
+  it('Should display sorted stores by 별점', async () => {
+    await SearchResultPage.sortBy('별점순');
+
+    let stores = await SearchResultPage.getStoresInfo();
+    console.log(stores);
+    expect(isDescendingArray(stores.map(store => store.star))).toBe(true);
+  });
+
+  
 });
 
 
